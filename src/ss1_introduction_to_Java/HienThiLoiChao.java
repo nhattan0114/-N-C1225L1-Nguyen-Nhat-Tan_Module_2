716f6478a4537737f54b5f770021069570394f0f
nhattan0114 <nhattan0114.fudn@gmail.com>
2026-03-19 18:42:25 +0700

    HienThiLoiChao

---
+++ src/ss1_introduction_to_Java/HienThiLoiChao.java
@@ -0,0 +1,13 @@
+package ss1_introduction_to_Java;
+
+import java.util.Scanner;
+
+public class HienThiLoiChao {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("Hello World "+name);
+
+}
+}
