ddb42bc8fafa6e3e5c4770ab9fad98a3326d8ad1
nhattan0114 <nhattan0114.fudn@gmail.com>
2026-03-19 19:38:51 +0700

    HienThiLoiChao

--- src/ss1_introduction_to_Java/HienThiLoiChao.java
+++ src/ss1_introduction_to_Java/HienThiLoiChao.java
@@ -7,7 +7,7 @@ public class HienThiLoiChao {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter your name: ");
         String name = sc.nextLine();
-        System.out.println("Hello World "+name);
+        System.out.println("Hello World hi"+name);
 
 }
 }
