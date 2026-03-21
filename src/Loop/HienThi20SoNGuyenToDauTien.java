4ad7e3d0fce9fa98122586274c8a59eee497db1d
nhattan0114 <nhattan0114.fudn@gmail.com>
2026-03-21 18:30:14 +0700

    Up

--- src/Loop/HienThi20SoNGuyenToDauTien.java
+++ src/Loop/HienThi20SoNGuyenToDauTien.java
@@ -1,4 +1,28 @@
 package Loop;
 
+import java.util.Scanner;
+
 public class HienThi20SoNGuyenToDauTien {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count =0;
+        System.out.print("Nhập số lượng số nguyên tố cần in: ");
+        int num=sc.nextInt();
+
+        int N=2;
+        while (count<num){
+            boolean isPrime =true;
+            for (int i = 2; i <=Math.sqrt(N) ; i++) {
+                    if (N%i==0){
+                        isPrime=false;
+                        break;
+                    }
+            }
+            if (isPrime){
+                System.out.println(N+ " ");
+                count++;
+            }
+          N++;
+        }
+    }
 }
