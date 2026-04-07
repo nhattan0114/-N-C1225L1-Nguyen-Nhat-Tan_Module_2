2455ceb536bf031f571a9a4766265cb05dfb7ae4
nhattan0114 <nhattan0114.fudn@gmail.com>
2026-04-07 15:18:34 +0700

    Up

---
+++ src/AccessModifier/TestCircle.java
@@ -0,0 +1,9 @@
+package AccessModifier;
+
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        System.out.println(circle.getRadius());
+        System.out.println(circle.getArea());
+    }
+}
